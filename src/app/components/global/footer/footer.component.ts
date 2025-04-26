e3403b65bffcbefc5ecf21ba4807cf81ab92999c
Charmants <charmants@noctaly.com>
2025-04-26 19:31:02 -0400

    FCT : footer

--- src/app/components/global/footer/footer.component.ts
+++ src/app/components/global/footer/footer.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  categories = [
+    {
+      title: "Site Web",
+      links: [
+        { title: "Accueil", url: "/" },
+        { title: "Services Offerts", url: "/services" },
+        { title: "Témoignages", url: "/temoignages" },
+      ],
+    },
+    {
+      title: "Services",
+      links: [
+        { title: "Service A", url: "/" },
+        { title: "Service B", url: "/" },
+        { title: "Service C", url: "/" },
+      ],
+    },
+    {
+      title: "Mentions légales",
+      links: [
+        { title: "Conditions d'utilisation", url: "/terms" },
+        { title: "Politique de confidentialité", url: "/privacy" },
+        { title: "Politique de cookies", url: "/cookies" },
+      ],
+    },
+  ];
 }
--- src/app/components/global/navbar/navbar.component.ts
+++ src/app/components/global/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, NgOptimizedImage],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
