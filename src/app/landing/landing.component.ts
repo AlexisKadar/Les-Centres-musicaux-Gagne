1cf2f5facb28265702b2d1394c9e4c7443739b09
David Bourgeois <1192146@ceegepmontpetit.ca>
2025-05-01 18:55:15 -0400

    FCT HistoryComponent

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../components/global/hero/hero.component';
+import { HistoryComponent } from '../history/history.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, HistoryComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
