86eb4d95cf39afbb7f426a5454ae84bb07a75ada
David Bourgeois <1192146@cegepmontpetit.ca>
2025-05-14 10:02:02 -0400

    FCT ajout de google maps sur la page succursales

--- src/app/succursales/succursales.component.ts
+++ src/app/succursales/succursales.component.ts
@@ -1,4 +1,4 @@
-import { NgOptimizedImage } from '@angular/common';
+import { NgFor, NgOptimizedImage } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps'
 import { LucideAngularModule } from 'lucide-angular';
@@ -6,11 +6,19 @@ import { LucideAngularModule } from 'lucide-angular';
 @Component({
   selector: 'app-succursales',
   standalone: true,
-  imports: [NgOptimizedImage, LucideAngularModule, GoogleMapsModule],
+  imports: [NgOptimizedImage, LucideAngularModule, GoogleMapsModule, NgFor],
   templateUrl: './succursales.component.html',
   styleUrls: ['./succursales.component.css']
 })
 export class SuccursalesComponent implements OnInit {
+
+  center : google.maps.LatLngLiteral = { lat: 45.49856712401489, lng: -73.51228043427608 };
+  zoom : number = 12;
+// 45.49856712401489, -73.51228043427608
+  markerPositions : google.maps.LatLngLiteral[] = [
+    { lat: 45.5111747, lng: -73.5623694},
+    { lat: 45.4852152, lng: -73.4677635 }
+  ];
 
   constructor() { }
 
