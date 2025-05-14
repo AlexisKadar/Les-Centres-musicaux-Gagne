4a70148e23c3321244f0ea8108aa738d1448258c
YannTu <6223399@cegepmontpetit.ca>
2025-05-14 00:56:01 -0400

    FCT : Partie du bas de Succursales

--- src/app/succursales/succursales.component.ts
+++ src/app/succursales/succursales.component.ts
@@ -1,9 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-succursales',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, LucideAngularModule],
   templateUrl: './succursales.component.html',
   styleUrls: ['./succursales.component.css']
 })
