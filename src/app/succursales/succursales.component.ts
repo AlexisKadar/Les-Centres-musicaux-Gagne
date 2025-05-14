4ab22bcb1d094fa9baab5940eb5311ccd87ec1cd
David Bourgeois <1192146@cegepmontpetit.ca>
2025-05-14 08:58:02 -0400

    Auto stash before merge of "main" and "origin/main"

--- src/app/succursales/succursales.component.ts
+++ src/app/succursales/succursales.component.ts
@@ -1,11 +1,12 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps'
 import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-succursales',
   standalone: true,
-  imports: [NgOptimizedImage, LucideAngularModule],
+  imports: [NgOptimizedImage, LucideAngularModule, GoogleMapsModule],
   templateUrl: './succursales.component.html',
   styleUrls: ['./succursales.component.css']
 })
